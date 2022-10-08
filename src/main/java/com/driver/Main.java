02b67679bc66474b4b0bb490088339cc9126f602
Eshwar <emiyyapuram@student.nitw.ac.in>
2022-10-08 17:42:17 +0530

    few changes

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -3,7 +3,7 @@ package com.driver;
 public class Main {
 
 
-       static class Product{
+       public static class Product{
          public int product(int x, int y) {
            return x*y;
          }
@@ -17,11 +17,12 @@ public class Main {
        }
 
   public static void main(String[] args) {
+
     Product p=new Product();
 
-    p.product(1,2);
-    p.product(1,2,3);
-    p.product(1.5,3.5);
+    System.out.println(p.product(1,2));
+    System.out.println(p.product(1,2,3));
+    System.out.println(p.product(1.5,3.5));
 
   }
   }
